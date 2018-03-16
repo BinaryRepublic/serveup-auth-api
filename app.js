aa7152da17f113b3f78677cc96b622179621b845
Henrik Engelbrink <hengel2810@gmail.com>
2018-03-16 17:28:42 +0100

    Added Access-Middleware

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const metricsMiddleware = promBundle({
+    includeStatusCode: true,
+    includeMethod: true,
+    includePath: true
+});
+app.use(metricsMiddleware);
+app.use('/', require('./src/middleware/accessControl').main);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
