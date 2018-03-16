98df71920bd34fe08811de77716903505c6a508d
Henrik Engelbrink <hengel2810@gmail.com>
2018-03-16 17:44:35 +0100

    Fixed bug

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const metricsMiddleware = promBundle({
-    includeStatusCode: true,
-    includeMethod: true,
-    includePath: true
-});
-app.use(metricsMiddleware);
 app.use('/', require('./src/middleware/accessControl').main);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
